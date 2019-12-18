baeea09de3463734a2657a9d65e51391b8fa7c2b
bo <boj@arcane.no>
2019-12-18 16:40:09 +0100

    update connect script to reflect new docker container names

--- scripts/connect-alice-bob.sh
+++ scripts/connect-alice-bob.sh
@@ -6,9 +6,9 @@ set -e
 # echo executed commands
 set -o xtrace
 
-BOB_IP=`docker inspect --format '{{json .NetworkSettings}}' lna-bob | jq .Networks.server_default.IPAddress --raw-output`
+BOB_IP=`docker inspect --format '{{json .NetworkSettings}}' la-bob-lnd | jq .Networks.server_default.IPAddress --raw-output`
 BOB_PUBKEY=`./lnd-bob getinfo | jq --raw-output .identity_pubkey`
-ALICE_IP=`docker inspect --format '{{json .NetworkSettings}}' lna-alice | jq .Networks.server_default.IPAddress --raw-output`
+ALICE_IP=`docker inspect --format '{{json .NetworkSettings}}' la-alice-lnd | jq .Networks.server_default.IPAddress --raw-output`
 
 ALICE_ADDR=`./lnd-alice newaddress p2wkh | jq --raw-output .address`
 BOB_ADDR=`./lnd-bob newaddress p2wkh | jq --raw-output .address`
